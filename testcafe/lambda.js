e77d854a14333b85bb329c34f215620327080a38
TestAndunR <andunranmal@gmail.com>
2018-09-10 15:11:19 +0530

    adding cognito with no reply mail address

--- testcafe/lambda.js
+++ testcafe/lambda.js
@@ -1,10 +1,11 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 let SL_AWS = require('slappforge-sdk-aws');
 
 exports.handler = function (event, context, callback) {
     cognito_idp.listUsers({
-        UserPoolId: process.env.UserPoolId_cognitoissueVALIDATE,
+        UserPoolId: process.env.UserPoolId_cognitoissueValidation,
         Limit: "10"
     }, function (error, data) {
         if (error) {
@@ -13,6 +14,7 @@ exports.handler = function (event, context, callback) {
         }
         // your logic goes within this block
     });
+
 
 
     callback(null, 'Successfully executed');
